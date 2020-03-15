1656e3245d33a1a6e3311b2352587553aed5efad
Ayse Turhan <teacherphysics03@gmail.com>
2020-03-15 21:05:12 +0200

    second commit from local repo

--- src/gitPackage1/GitDay3.java
+++ src/gitPackage1/GitDay3.java
@@ -3,8 +3,10 @@ package gitPackage1;
 public class GitDay3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("My first changes on local master");
+		
+		
 	}
 
 }
